f9b8877e0334869213a82f7f7cee871be0a57efc
HrCh007 <chowdhryhrithik@gmail.com>
2022-07-27 14:13:38 +0530

    second commit

--- Practice.py
+++ Practice.py
@@ -23,3 +23,30 @@ print(cars)
 
 cars.remove("BMW")
 print(cars)
+
+#Python Classes and Objects
+
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+class Person:
+  def __init__(self, name, age):              #__init__() function is called automatically when a new object is created.
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+p1.age = 40
+print(p1.age)
+
+del p1.age
+
+del p1
+
